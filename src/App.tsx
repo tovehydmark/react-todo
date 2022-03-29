91eb1441f16da258f7205e29f87646a671087041
tovehydmark <tovehydmark@gmail.com>
2022-03-29 17:25:33 +0200

    added add todo

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { Todos } from "./components/Todos";
+import { AddTodo } from "./components/AddTodo";
 
 function App() {
   return (
     <div className="App">
+      <AddTodo></AddTodo>
       <Todos></Todos>
     </div>
   );
--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,3 +1,9 @@
+import { Todo } from "../models/Todo";
+
+interface IAddProps {
+  addTodoFromChild(todo: Todo): void;
+}
+
 export function AddTodo() {
   return <>AddTodo works!</>;
 }
